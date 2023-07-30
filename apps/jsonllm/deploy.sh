a898e5e0fc9f86ec2606db56ced13ade3ad9bd0b
Jerome Swannack <jeromeswannack@gmail.com>
2023-07-30 20:02:47 +1200

    Added service and ingress

--- apps/jsonllm/deploy.sh
+++ apps/jsonllm/deploy.sh
@@ -1,2 +1,7 @@
 kubectl create namespace jsonllm
-kubectl apply -n jsonllm -f deployment.yaml
+kubectl apply -n jsonllm \
+    -f deployment.yaml \
+    -f service.yaml \
+    -f ingress.yaml \
+    -f pv.yaml \
+    -f pvc.yaml
