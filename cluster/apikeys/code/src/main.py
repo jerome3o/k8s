a73a679d0ad9d132e35f6967956b0e665112bef3
Jerome Swannack <jeromeswannack@gmail.com>
2023-08-27 17:08:21 +1200

    Added check for x-api-key

--- cluster/apikeys/code/src/main.py
+++ cluster/apikeys/code/src/main.py
@@ -1,26 +1,11 @@
-from typing import Optional
-from fastapi import FastAPI, Request
-from pydantic import BaseModel
+from fastapi import FastAPI, Request, HTTPException
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    content: Optional[str] = None
-
-
 @app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
-async def root(path: str, request: Request, item: Optional[Item] = None):
-    client_host = request.client.host
-    method = request.method
-    url_path = request.url.path
-    query_params = dict(request.query_params)
+async def root(path: str, request: Request):
+    if "x-api-key" not in request.headers:
+        raise HTTPException(status_code=401, detail="Missing API key")
 
-    print(f"Client Host: {client_host}")
-    print(f"HTTP Method: {method}")
-    print(f"URL Path: {url_path}")
-    print(f"Query Params: {query_params}")
-    if item:
-        print(f"Body: {item.model_dump()}")
-
-    return {"status_code": 200, "message": "OK"}
+    return
