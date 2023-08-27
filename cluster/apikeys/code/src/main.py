93e9976cf1f8bb3f889a713d278468c526c9e1b0
Jerome Swannack <jeromeswannack@gmail.com>
2023-08-28 10:34:08 +1200

    Inserting api keys as env var

--- cluster/apikeys/code/src/main.py
+++ cluster/apikeys/code/src/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI, Request, HTTPException
+import os
 
 app = FastAPI()
+
+API_KEYS = set(os.environ["API_KEYS"].split(","))
 
 
 @app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
 async def root(path: str, request: Request):
     if "x-api-key" not in request.headers:
         raise HTTPException(status_code=401, detail="Missing API key")
+
+    if request.headers["x-api-key"] not in API_KEYS:
+        raise HTTPException(status_code=401, detail="Invalid API key")
 
     return
