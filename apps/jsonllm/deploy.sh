5fc42bd369446ff92ffe337ea02ada8935ecbf32
Jerome Swannack <jeromeswannack@gmail.com>
2023-08-28 17:08:31 +1200

    Added api key auth to jsonllm

--- apps/jsonllm/deploy.sh
+++ apps/jsonllm/deploy.sh
@@ -1,8 +1,3 @@
 kubectl create namespace jsonllm
-kubectl apply -n jsonllm \
-    -f manifests/namespace.yaml \
-    -f manifests/deployment.yaml \
-    -f manifests/service.yaml \
-    -f manifests/ingress.yaml \
-    -f manifests/pv.yaml \
-    -f manifests/pvc.yaml
+kubectl apply -n jsonllm -f manifests/
+kubectl apply -n jsonllm -f auth/
