ccf18806372507b0a3017fda29a015971498ab6d
Jerome Swannack <jeromeswannack@gmail.com>
2023-08-30 19:03:12 +1200

    Reverted auth attempt for daily

--- apps/panopticode/daily/deploy.sh
+++
@@ -1,7 +0,0 @@
-helm repo add oauth2-proxy https://oauth2-proxy.github.io/manifests
-
-kubectl apply -f be/
-kubectl apply -f fe/auth/manifests/
-kubectl apply -f fe/manifests/
-
-helm upgrade --install daily-auth oauth2-proxy/oauth2-proxy -n panopticode -f fe/auth/values.yaml
