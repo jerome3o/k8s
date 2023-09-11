d9387f9d2b2a11b141bb0849188b435f669a290b
Jerome Swannack <jeromeswannack@gmail.com>
2023-09-11 20:48:38 +1200

    Using values.yaml in nc deploy script

--- apps/nextcloud/deploy.sh
+++ apps/nextcloud/deploy.sh
@@ -2,4 +2,4 @@ helm repo add nextcloud https://nextcloud.github.io/helm/
 
 kubectl apply -f manifests/
 
-helm upgrade --install nextcloud nextcloud/nextcloud -n nextcloud --create-namespace
+helm upgrade --install nextcloud nextcloud/nextcloud -n nextcloud --create-namespace -f values.yaml
